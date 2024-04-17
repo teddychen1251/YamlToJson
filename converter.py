cc2bd92957daef974465596d0f1d452665d01067
Teddy Chen <teddychen1251@gmail.com>
2024-04-17 01:15:52 -0700

    Implement arg parsing

--- converter.py
+++ converter.py
@@ -1,3 +1,10 @@
-f = open("input/simple_string.yaml")
-print(f.read())
-f.close()
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("file")
+args = parser.parse_args()
+
+yaml = open(args.file)
+base_name = yaml.name.split("/")[-1].split(".")[0]
+print(base_name)
+yaml.close()
